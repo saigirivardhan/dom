dacdbdca668615872838e83d2f87e9a1776a61fc
saigirivardhan <102036605+saigirivardhan@users.noreply.github.com>
2022-06-07 20:42:49 +0530

    Add files via upload

---
+++ as.js
@@ -0,0 +1,41 @@
+// let x = document.getElementById("test")
+// console.log(test)
+// setTimeout(we,5000)
+// function w2(){
+//     x.style.display = "inline-flex"
+// }
+
+// function we(){
+//     x.style.display = "none"
+// }
+
+// const x = document.getElementsByTagName("img")
+// console.log(x)
+// setTimeout(w3,2000)
+// function w3(){
+//     x[1].style.display = "flex"
+// }
+// setTimeout(()=>{
+//     x[1].style.display = "none"
+// },3000)
+
+// const x = document.getElementsByTagName("button")
+// console.log(x)
+// function w4(){
+//     x[0].style.display = "none"   //button disappears
+    
+// }
+
+// const x = document.getElementsByTagName("div")
+// console.log(x)
+// function w4(){
+//     x[1].style.display = "none"   //box disappears
+    
+// }
+
+// const x = document.getElementsByTagName("div")
+// console.log(x)
+// function w4(){
+//     x[0].style.display = "none"   //totally disappears
+    
+// }
